57b4b63501f13759b5f19cdcaf64b5afd33ce510
SebaTrabajo <sebastian.lanzetti@hotmail.com>
2023-07-11 12:06:58 -0300

    Intentando agregar modal

--- 10_trabajos/3ra_entrega/javascript/vista/vista.js
+++ 10_trabajos/3ra_entrega/javascript/vista/vista.js
@@ -20,9 +20,8 @@ let txtAreaClavesGeneradas = document.getElementById("idClavesGeneradasTextArea"
 //Código
 document.getElementById("idBtnGenerarClave").addEventListener("click", function(event) {
     event.preventDefault(); // Evita el comportamiento predeterminado del botón
-    let cantidad = document.getElementById("TxtCantidad").value;
-    console.log(cantidad);
-    let claves_a_generar = objCantidad.validar_cantidad_claves(cantidad);
+    let cantidad = document.getElementById("TxtCantidad").value;//valor de la caja de texto
+    let claves_a_generar = objCantidad.validar_cantidad_claves(cantidad);//llamo función validar
 
         if (claves_a_generar === true){
             /*Devuelve un Verdadero cuando la cantidad es un número válido, es decir, un número natural (n° positivo). Ingresa en este if
@@ -40,24 +39,20 @@ document.getElementById("idBtnGenerarClave").addEventListener("click", function(
                         longitud_clave[i] = prompt('Ingrese logitud de clave, mínimo 7 máximo 20');
                         i++;
                     }
-
+                    const numerosLista = document.getElementById("numeros-lista");
+                    const numeroItem=[];
+                    
                     for (let i = 0; i < longitud_clave.length; i++) {
+                        
                         /*Al incovar la funcion validar_longitud, valido las longitudes ingresadas.
                         Si se encuentran dentro de los parametros arroja una clave aleatoria
                         si no, arroja la excepción que pueden ser varias.*/
-                        array_de_claves += `${i+1}) ${objClave.validar_longitud(longitud_clave[i])}\n`;
+                        array_de_claves[i] += `${i+1})${objClave.validar_longitud(longitud_clave[i])}\n`;
+                        numeroItem[i] = document.createElement("li");
+                        numeroItem[i].textContent = array_de_claves[i];
+                        numerosLista.appendChild(numeroItem[i]);          //test     
                     }
-                    //Acá muestro en un alert las claves / excepciones arrojadas.
-                    if (cantidad.length === 1) {
-                        //Si la cantidad de claves a generar es 1, modifico el texto.
-                        cantidadClaves.innerText = "Clave generada";
-                    } else {
-                        // nada
-                    }
-                    //Muestro las claves si en el primer intento, el usuario ingresó un valor válido.
-                    //alert(array_de_claves);
-                    //Muestro en el text area las claves/excepciones generadas
-                    txtAreaClavesGeneradas.innerHTML = array_de_claves;
+
         }
         else if (claves_a_generar != true) {
             while (claves_a_generar != true) {
@@ -78,7 +73,6 @@ document.getElementById("idBtnGenerarClave").addEventListener("click", function(
                         longitud_clave[i] = prompt('Ingrese logitud de clave, mínimo 7 máximo 20');
                         i++;
                     }
-
                     for (let i = 0; i < longitud_clave.length; i++) {
                         /*Genero un array con los resultados que arroja el objeto "objClave"
                         puede ser una clave generada o una excepción. agrego \n para imprimir
@@ -88,7 +82,11 @@ document.getElementById("idBtnGenerarClave").addEventListener("click", function(
                     /*Acá muestro las claves SI en el primer intento de cantidad de claves fue falso. es decir: 
                     1er intento = asdasd (false) | otroIntento  = verdadero*/
                     //Muestro en el text area las claves/excepciones generadas
-                    txtAreaClavesGeneradas.innerHTML = array_de_claves;
+                    const numeroItem = document.createElement("li");
+                    numeroItem.textContent = array_de_claves;
+                    numerosLista.appendChild(numeroItem);
+
+                    //txtAreaClavesGeneradas.innerHTML = array_de_claves;
                 }
             }
         }
